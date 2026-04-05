8956b2a7ae4d440c51d5aadf38d4429f80c5fbc1
adish17 <adishirfan17@gmail.com>
2026-04-06 01:02:28 +0530

    state hoisting

--- Composition and Recomposition/app/src/main/java/com/androidstudioprojects/compositionandrecomposition/MainActivity.kt
+++ Composition and Recomposition/app/src/main/java/com/androidstudioprojects/compositionandrecomposition/MainActivity.kt
@@ -5,18 +5,20 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.androidstudioprojects.compositionandrecomposition.ui.theme.CompositionAndRecompositionTheme
 
@@ -27,7 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CompositionAndRecompositionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Counter(modifier = Modifier.padding(innerPadding))
+                    Screen1(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -35,18 +37,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Counter(modifier: Modifier) {
+fun Screen1(modifier: Modifier) {
+    Column(modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(20.dp)) {
+
+        var counter by rememberSaveable {mutableStateOf(0)}
+
+        Counter(counter) {
+            counter++
+        }
+    }
+}
+
+@Composable
+fun Counter(counter: Int, onValueChanged: () -> Unit) {
     Log.d("Recompose", "Enter counter")
 
-    var counter = rememberSaveable {mutableStateOf(0)}
-
-    Column(modifier = modifier.padding(10.dp)) {
+    Column(modifier = Modifier.padding(10.dp)) {
         Text(text = "Static Title")
 
-        Text(text = "Count : ${counter.value}")
-        Log.d("Recompose", "Counter : ${counter.value}")
+        Text(text = "Count : $counter")
+        Log.d("Recompose", "Counter : $counter")
 
-        Button({counter.value++}) {
+        Button({onValueChanged()}) {
             Text(text = "Click")
         }
     }
