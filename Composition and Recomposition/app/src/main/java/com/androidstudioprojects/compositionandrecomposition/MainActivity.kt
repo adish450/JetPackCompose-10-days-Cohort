910fd900a51356120b4e7bb66683606b1009ca98
adish17 <adishirfan17@gmail.com>
2026-04-05 21:53:36 +0530

    using remember to store the value of state in slot table

--- Composition and Recomposition/app/src/main/java/com/androidstudioprojects/compositionandrecomposition/MainActivity.kt
+++ Composition and Recomposition/app/src/main/java/com/androidstudioprojects/compositionandrecomposition/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +37,7 @@ class MainActivity : ComponentActivity() {
 fun Counter(modifier: Modifier) {
     Log.d("Recompose", "Enter counter")
 
-    var counter = mutableStateOf(0)
+    var counter = remember {mutableStateOf(0)}
 
     Column(modifier = modifier.padding(10.dp)) {
         Text(text = "Static Title")
