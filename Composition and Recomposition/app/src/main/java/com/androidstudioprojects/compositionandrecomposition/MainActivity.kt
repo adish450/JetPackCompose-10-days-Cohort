211fae28f120aefaa2f479248e7bffb75af40d08
adish17 <adishirfan17@gmail.com>
2026-04-05 21:55:16 +0530

    using rememberSavable to save state in SavedStateRegistry for recreation of slot table with saved state data

--- Composition and Recomposition/app/src/main/java/com/androidstudioprojects/compositionandrecomposition/MainActivity.kt
+++ Composition and Recomposition/app/src/main/java/com/androidstudioprojects/compositionandrecomposition/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,7 +38,7 @@ class MainActivity : ComponentActivity() {
 fun Counter(modifier: Modifier) {
     Log.d("Recompose", "Enter counter")
 
-    var counter = remember {mutableStateOf(0)}
+    var counter = rememberSaveable {mutableStateOf(0)}
 
     Column(modifier = modifier.padding(10.dp)) {
         Text(text = "Static Title")
