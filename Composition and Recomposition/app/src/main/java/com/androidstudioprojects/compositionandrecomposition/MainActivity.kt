f7f3efce0b371d5923cf3cec84010d6c25b26469
adish17 <adishirfan17@gmail.com>
2026-04-07 01:26:55 +0530

    using derviedStateOf

--- Composition and Recomposition/app/src/main/java/com/androidstudioprojects/compositionandrecomposition/MainActivity.kt
+++ Composition and Recomposition/app/src/main/java/com/androidstudioprojects/compositionandrecomposition/MainActivity.kt
@@ -12,16 +12,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.androidstudioprojects.compositionandrecomposition.ui.theme.CompositionAndRecompositionTheme
 
@@ -76,6 +83,25 @@ fun Counter(counter: Int, onValueChanged: () -> Unit) {
             value = email,
             onValueChange = {email = it},
             label = {Text(text = "Email")})
+
+        val isEnable by remember { derivedStateOf { email.isEmpty() } }
+
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+                .padding(vertical = 20.dp),
+            shape = RoundedCornerShape(20.dp),
+                colors = if (isEnable) ButtonDefaults.
+            buttonColors(
+                containerColor = Color.Black,
+                contentColor = Color.White
+            ) else ButtonDefaults.buttonColors(
+                containerColor = Color.LightGray,
+                contentColor = Color.White
+            )
+        ){
+            Text(text = "Submit")
+        }
 
     }
 }
