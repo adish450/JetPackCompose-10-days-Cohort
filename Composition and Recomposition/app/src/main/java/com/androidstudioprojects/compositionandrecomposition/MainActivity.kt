c46f45710da0386520deaca549c6b0387f0e80c9
adish17 <adishirfan17@gmail.com>
2026-04-07 01:08:56 +0530

    OutlinedTextField (EditText)

--- Composition and Recomposition/app/src/main/java/com/androidstudioprojects/compositionandrecomposition/MainActivity.kt
+++ Composition and Recomposition/app/src/main/java/com/androidstudioprojects/compositionandrecomposition/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -62,5 +65,17 @@ fun Counter(counter: Int, onValueChanged: () -> Unit) {
         Button({onValueChanged()}) {
             Text(text = "Click")
         }
+
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(20.dp))
+
+        var email by rememberSaveable { mutableStateOf("")}
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            label = {Text(text = "Email")})
+
     }
 }
